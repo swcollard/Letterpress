83449b86c7e95fd1eaeb9d8a35448ade63607a34
Sam Collard <swcollard12@gmail.com>
2013-08-17 13:34:37 -0700

    make python 3.2 the default

--- code/letterpress.py
+++ code/letterpress.py
@@ -1,9 +1,9 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python3.2
 # -*- coding: utf-8 -*-
 
-__version_info__ = (0, 0, 2)
+__version_info__ = (0, 0, 3)
 __version__ = '.'.join(map(str, __version_info__))
-__author__ = "Ling Wang"
+__author__ = "Ling Wang, forked by Sam Collard"
 
 import sys
 
